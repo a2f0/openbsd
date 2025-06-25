b95eed83a690a2a0cd8e3cb01284c412395d1066
Dan Sullivan <dansullivan@gmail.com>
2025-06-24 21:52:38 -0400

    feat: forward networking

--- qemu.sh
+++ qemu.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Get host IP for QEMU networking
+HOST_IP=$(ip route get 1.1.1.1 | awk '{print $7; exit}')
+
 # Start Python web server in background
 echo "Starting Python web server on port 8686..."
 python3 -m http.server 8686 &
@@ -39,8 +42,9 @@ fi
 # create and format the disk partition
 qemu-img create -f qcow2 openbsd-vm.qcow2 2G
 
-# The cleanup function will be called automatically when the script exits
-echo "Script completed. Web server will be stopped automatically."
+isoinfo -i $ISO_FILE -R -x /$OPENBSD_VERSION/amd64/bsd.rd > bsd.rd
+
+echo "Starting QEMU with network access to host IP: $HOST_IP"
 
 qemu-system-x86_64 \
   -m 2048 \
@@ -48,6 +52,9 @@ qemu-system-x86_64 \
   -cdrom $ISO_FILE \
   -hda openbsd-vm.qcow2 \
   -boot d \
-  -netdev user,id=mynet0 -device e1000,netdev=mynet0 \
-  -kernel /7.7/amd64/bsd.rd \
-  -append "com0=/dev/ttyS0 console=com0 autoinstall=http://<host-ip>:8000/install.conf"
+  -netdev user,id=mynet0,hostfwd=tcp::8686-:8686 -device e1000,netdev=mynet0 \
+  -kernel bsd.rd \
+  -append "com0=/dev/ttyS0 console=com0 autoinstall=http://$HOST_IP:8686/install.conf"
+
+# The cleanup function will be called automatically when the script exits
+echo "Script completed. Web server will be stopped automatically."
